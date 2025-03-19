250de429c6e26ec73247df13f87d0ada05616f82
Peter Buchmann <peter.buchmann@web.de>
2025-03-19 11:48:29 +0100

    separate extraction of table contents and any other contents - 0.2

--- lib/pdf-extraction-02.py
+++ lib/pdf-extraction-02.py
@@ -9,10 +9,20 @@ import pandas as pd
 
 
 import streamlit as st
-
+st.set_page_config(
+    page_title=None, 
+    page_icon=None, 
+    # layout="centered", 
+    layout="wide", 
+    # initial_sidebar_state="auto", 
+    initial_sidebar_state="collapsed", 
+    menu_items=None,
+)
+print("\nrun start")
 
 import fitz  # PyMuPDF
 import pdfplumber
+import pdfminer.high_level
 
 from collections import defaultdict
 
@@ -65,7 +75,7 @@ def showDF(df: pd.DataFrame, pageNum: int, tableNum: int):
 
 
 # using pdf plumber
-def extractTables(pg, pageNum=0):
+def UnusedextractTables(pg, pageNum=0):
 
     tables = pg.extract_tables()
     for tableNum, table in enumerate(tables):
@@ -95,32 +105,59 @@ def extractTables(pg, pageNum=0):
 
 
 
+# Dedupe text inside table bounding boxes
 
-def processFile(pth: Path, limitPages=10, limitOther=2222):
+def filterTableContents(pdf, tableBBoxesDoc):
 
-    lastX2 = lastXDirs(pth, 2)
-    lastX4 = lastXDirs(pth, 4)
+    filteredDoc = []
+    
+    for pageNum, pg in enumerate(pdf.pages):
 
-    print(f"pth = ..{os.sep}{lastX4}")
-    st.write(f"### file ..{os.sep}{lastX2}")
+        if pageNum > limitPages:
+            break
 
-    with pdfplumber.open(pth) as pdf:
+        filteredPg = []
+
+        words = pg.extract_words()  # with coordinates
+        # st.write(f"page {pageNum} - words {len(words)}")
         
-        for pgNum, pg in enumerate(pdf.pages):
-            if pgNum > limitPages:
-                break
-            extractTables(pg, pgNum)
+        tableBBsPg = tableBBoxesDoc[pageNum]
 
-        # see pdf-extraction-01.py for more extraction logic - but useless
+        for wordNum, w in enumerate(words):
+            wbb = (w["x0"], w["top"], w["x1"], w["bottom"]) # word bounding box
+        
+            # check if word is inside any table's bounding box
+            contains = False
+            for tbb in tableBBsPg:
+                if tbb[0] <= wbb[0] <= tbb[2]   and    tbb[1] <= wbb[1] <= tbb[3]  :
+                    contains = True
+                    break
+            
+            if not contains:
+                filteredPg.append(w["text"])
+
+
+        s = " ".join(filteredPg)
+        filteredDoc.append(s)
+
+        st.write(f"page {pageNum} - words {len(words)} - filtered {len(filteredPg)}")
+
+
+    return filteredDoc
 
 
 
+def containsAnyText(pdf):
+    for pgNum, pg in enumerate(pdf.pages):
+        if pg.extract_text():
+            return True  # is extractable
+    return False  # no text detected, likely scanned
 
 
+workDir = Path.cwd() / "cbcr"
 
-
-
-
+limitPages=9
+limitOther=2222
 
 files = [
     "AT_Oberbank_AG_2016_cbcr_ar_en-page-146-148.pdf",
@@ -129,9 +166,83 @@ files = [
     "AT_Oberbank_AG_2016_cbcr_ar_en.pdf",
 ]
 
-workDir = Path.cwd() / "cbcr"
 
 for fn in files:
     pth = workDir / fn
-    processFile(pth)
-    # MuPdfProcessFile(pth)
+
+    lastX2 = lastXDirs(pth, 2)
+    lastX4 = lastXDirs(pth, 4)
+
+    print(f"pth = ..{os.sep}{lastX4}")
+    st.write(f"### file ..{os.sep}{lastX2}")
+
+    with pdfplumber.open(pth) as pdf:        
+
+        if not containsAnyText(pdf):
+            st.write(f"only scanned contentss")
+            continue
+
+
+        if False:
+            structuredText = pdfminer.high_level.extract_text(pth)
+            st.write(f"{structuredText[:200]}... \n\n")
+            print(f"{structuredText[:200]}... \n\n")
+
+
+        tables = []  # tables whole doc
+        bBoxes = []  # tables whole doc - bounding boxes 
+
+        for pageNum, pg in enumerate(pdf.pages):
+            # st.write(f" {pgNum=} {pg=}  ")
+            if pageNum > limitPages:
+                break
+
+            tablesPg = [] # tables per page
+            bboxesPg = [] # tables per page - bounding boxes 
+            ts = pg.extract_tables()
+            for tableNum, t in enumerate(ts):
+                # print(f"\tt{tableNum:3>}  ")
+                df = pd.DataFrame(t[1:], columns=t[0])  # to DataFrame
+                # unique column names
+                df.columns = uniqueCols(df.columns)
+                tablesPg.append(
+                    {
+                        "pageNum":  pageNum  + 1, 
+                        "tableNum": tableNum + 1, 
+                        "df":       df,
+                        "table":    df.to_dict(orient='records'),
+                    },
+                )
+
+                # table bounding box to exclude text
+                bbox = pg.find_tables()[tableNum].bbox
+                bboxesPg.append(bbox)
+
+            tables.append(tablesPg)
+            bBoxes.append(bboxesPg)
+
+            if len(tablesPg) > 0:
+                st.write(f" page {pageNum}: tables {len(tablesPg)} - bounding boxes {len(bboxesPg)}")
+            
+
+
+            # extractTables(pg, pgNum)
+
+
+        # remove table text from extracted text
+        cleanTexts = filterTableContents(pdf,  bBoxes)
+
+        for pg, cleanText in enumerate(cleanTexts):
+
+            st.write(f'''
+                #### cleanText for page {pg+1}
+                {cleanText[:400]}...
+            ''')
+
+
+        if False:
+            for ti in tables:
+                showDF( ti["df"], ti["pageNum"], ti["tableNum"], )
+
+
+print("run stop\n")
